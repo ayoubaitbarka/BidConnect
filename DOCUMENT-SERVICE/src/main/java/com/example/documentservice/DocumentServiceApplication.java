c204923b663b1088b567d85a7bdffa495e47fbce
0lafwan <nawfaltq@gmail.com>
2025-12-28 17:26:22 +0100

    test de connectivité avec MinIo

--- DOCUMENT-SERVICE/src/main/java/com/example/documentservice/DocumentServiceApplication.java
+++ DOCUMENT-SERVICE/src/main/java/com/example/documentservice/DocumentServiceApplication.java
@@ -1,7 +1,11 @@
 package com.example.documentservice;
 
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DocumentServiceApplication {
@@ -10,4 +14,14 @@ public class DocumentServiceApplication {
         SpringApplication.run(DocumentServiceApplication.class, args);
     }
 
+    // ✅ Test de connectivité avec MinIO
+    @Bean
+    public CommandLineRunner testMinio(MinioClient minioClient, @Value("${minio.bucket}") String bucket) {
+        return args -> {
+            boolean exists = minioClient.bucketExists(
+                    io.minio.BucketExistsArgs.builder().bucket(bucket).build()
+            );
+            System.out.println("Le bucket '" + bucket + "' existe ? " + exists);
+        };
+    }
 }
